da4f50f6ed60d602936eabdf0e586f4a3f5e9449
MrSpeedy68 <47033681+MrSpeedy68@users.noreply.github.com>
2020-02-12 19:59:54 +0000

    FXML file

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
+
 public class Controller {
+
+    @FXML
+    ImageView ImageViewMain;
+    @FXML
+    MenuItem OpenFile;
+    @FXML
+    MenuItem Quit;
+
+
+
+
+
+
+
 }
