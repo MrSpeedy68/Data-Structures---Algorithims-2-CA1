e0ead7d721cbb47696a458d953b402256f875034
MrSpeedy68 <mrspeedygamer123@gmail.com>
2020-03-11 14:55:09 +0000

    RecognisionTab

--- src/sample/RecognisionController.java
+++ src/sample/RecognisionController.java
@@ -2,15 +2,34 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
 import javafx.scene.paint.Color;
 
 import javax.swing.*;
+import java.io.IOException;
 
 public class RecognisionController {
 
     public int[] BloodCell = new int[16384];
     private PixelReader pixelReader;
+
+    @FXML
+    Button BloodCellBtn;
+    @FXML
+    ImageView ImageViewTri;
+    @FXML
+    MenuItem Quit;
+    @FXML
+    MenuItem MainWindow;
+
+
+
 
     @FXML
     public void AnalyiseImg(ActionEvent e) {
@@ -28,4 +47,22 @@ public class RecognisionController {
             }
         }
     }
+
+    //Exit Program Method
+    @FXML
+    public void ExitProgram(ActionEvent e) {
+        Runtime.getRuntime().exit(0);
+    }
+
+    @FXML
+    public void MainWindowScene(ActionEvent e) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+            Main.mainWindow.setScene(new Scene(root));
+        } catch (IOException el) {
+            el.printStackTrace();
+        }
+    }
+
+
 }
